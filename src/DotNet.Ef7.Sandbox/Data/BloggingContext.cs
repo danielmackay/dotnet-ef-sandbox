8be8ad80805dacfae0b4bccf65ebd8797f172622
Daniel Mackay <daniel.mackay@live.com>
2022-11-12 15:25:11 +1000

    🔊 added EF logs

--- src/DotNet.Ef7.Sandbox/Data/BloggingContext.cs
+++ src/DotNet.Ef7.Sandbox/Data/BloggingContext.cs
@@ -1,23 +1,32 @@
 using DotNet.Ef7.Sandbox.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace DotNet.Ef7.Sandbox.Data;
 
 public class BloggingContext : DbContext
 {
     private readonly IConfiguration _configuration;
+    private readonly ILogger<BloggingContext> _logger;
 
     public DbSet<Blog> Blogs { get; set; }
     public DbSet<Post> Posts { get; set; }
 
-    public BloggingContext(IConfiguration configuration)
+    public BloggingContext(IConfiguration configuration, ILogger<BloggingContext> logger)
     {
         _configuration = configuration;
+        _logger = logger;
     }
 
     // The following configures EF to create a Sqlite database file in the
     // special "local" folder for your platform.
     protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseSqlServer(_configuration.GetConnectionString("Default"));
+    {
+        options.UseSqlServer(_configuration.GetConnectionString("Default"));
+
+        options.EnableDetailedErrors();
+        options.EnableSensitiveDataLogging();
+        options.LogTo(msg => _logger.LogDebug(msg));
+    }
 }
