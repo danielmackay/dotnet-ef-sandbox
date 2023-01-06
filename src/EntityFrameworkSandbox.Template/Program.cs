447e6ec6d4a02259cd8e7ffbbee4746850ce2632
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-01-07 09:59:00 +1000

    Add spectre console

--- src/EntityFrameworkSandbox.Template/Program.cs
+++ src/EntityFrameworkSandbox.Template/Program.cs
@@ -1,10 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-using EntityFrameworkSandbox.Template;
+﻿using EntityFrameworkSandbox.Template;
+using EntityFrameworkSandbox.Template.Cli;
 using EntityFrameworkSandbox.Template.Data;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Spectre.Console;
+using Spectre.Console.Cli;
+
+AnsiConsole.Write(new FigletText("EF Sandbox").Color(Color.Purple));
+// TODO DM: Dynamically pull version
+AnsiConsole.WriteLine("Entity Framework Sandbox Command-line Tools 0.0.1");
+AnsiConsole.WriteLine();
 
 var builder = Host.CreateDefaultBuilder(args);
+
+// Add services to the container
 builder.ConfigureServices(services =>
 {
     services.AddDbContext<BloggingContext>();
@@ -12,14 +21,43 @@ builder.ConfigureServices(services =>
     services.AddTransient<Sandbox>();
 });
 
-var app = builder.Build();
-app.Start();
-
-using (var scope = app.Services.CreateScope())
+var host = builder.Build();
+using (var scope = host.Services.CreateScope())
 {
-    var sandbox = app.Services.GetRequiredService<Sandbox>();
-    await sandbox.Run();
+    var initialiser = host.Services.GetRequiredService<BloggingContextInitialiser>();
 
-    Console.WriteLine("Done!"); 
-    Console.ReadKey();
+    AnsiConsole.WriteLine("Initializing DB");
+    await initialiser.InitialiseAsync();
+
+    AnsiConsole.WriteLine("Seeding DB");
+    await initialiser.SeedAsync();
+
+    AnsiConsole.WriteLine("Done!");
+}
+
+var registrar = new TypeRegistrar(builder);
+
+var app = new CommandApp(registrar);
+
+app.Configure(config =>
+{
+    config.PropagateExceptions();
+
+    //// Register available commands
+    //config.AddCommand<WeatherForecastCommand>("forecasts")
+    //    .WithDescription("Display local weather forecasts.")
+    //    .WithExample(new[] { "forecasts", "5" });
+});
+
+// TODO: Migrate to command
+
+
+try
+{
+    return app.Run(args);
+}
+catch (Exception ex)
+{
+    AnsiConsole.WriteException(ex, ExceptionFormats.ShortenEverything);
+    return -99;
 }
