33d556b2bb9df20ef67a0d29832fe5119fd8e07f
Daniel Mackay [SSW] <2636640+danielmackay@users.noreply.github.com>
2023-01-07 11:04:12 +1000

    Changed to file scoped namespaces

--- src/EntityFrameworkSandbox.Template/Cli/Commands/GetBlogsCommand.cs
+++ src/EntityFrameworkSandbox.Template/Cli/Commands/GetBlogsCommand.cs
@@ -3,29 +3,28 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Spectre.Console.Cli;
 
-namespace EntityFrameworkSandbox.Template.Cli.Commands
+namespace EntityFrameworkSandbox.Template.Cli.Commands;
+
+public class GetBlogsCommand : AsyncCommand
 {
-    public class GetBlogsCommand : AsyncCommand
+    private readonly BloggingContext _db;
+    private readonly ILogger<GetBlogsCommand> _logger;
+
+    public GetBlogsCommand(BloggingContext context, ILogger<GetBlogsCommand> logger)
     {
-        private readonly BloggingContext _db;
-        private readonly ILogger<GetBlogsCommand> _logger;
+        _db = context;
+        _logger = logger;
+    }
 
-        public GetBlogsCommand(BloggingContext context, ILogger<GetBlogsCommand> logger)
-        {
-            _db = context;
-            _logger = logger;
-        }
+    public override async Task<int> ExecuteAsync(CommandContext context)
+    {
+        _logger.LogInformation("Getting Blogs...");
 
-        public override async Task<int> ExecuteAsync(CommandContext context)
-        {
-            _logger.LogInformation("Getting Blogs...");
+        var blogs = await _db.Blogs.ToListAsync();
 
-            var blogs = await _db.Blogs.ToListAsync();
+        foreach (var blog in blogs)
+            _logger.LogInformation(blog.ToString());
 
-            foreach (var blog in blogs)
-                _logger.LogInformation(blog.ToString());
-
-            return 0;
-        }
+        return 0;
     }
 }
